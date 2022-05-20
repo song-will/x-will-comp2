090cb816e692dbe0ee656bc974f513d67d9d59bf
zhuqingsong <735560190@qq.com>
2022-05-21 06:49:05 +0800

    递归组件

--- src/views/ComponentTest.vue
+++ src/views/ComponentTest.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="component-test">
-    <h2>组件</h2>
-    <s-tree></s-tree>
+    <h2>递归组件</h2>
+    <s-tree :options="treeData"></s-tree>
   </div>
 </template>
 
 <script setup>
 import STree from '@/components/STree.vue';
+import { treeData } from '@/test-consts/'
 
 </script>
