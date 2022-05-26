72d664acfbe47e72a0c9624de786c1fee1acbdd6
zhuqingsong <735560190@qq.com>
2022-05-26 10:48:18 +0800

    install

--- src/components/s-tree/index.js
+++ src/components/s-tree/index.js
@@ -1,10 +1,10 @@
 import STree from './STree.vue'
 
-STree.install = (app) => {
-    app.component(STree.name, STree)
+export default {
+    install: (app) => {
+        app.component(STree.name, STree)
+    }
 }
-
-export default STree
 
 
  
