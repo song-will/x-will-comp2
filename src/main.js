397bcb8d008f534dc89f8b59193720506a0cb22f
song-will <735560190@qq.com>
2022-07-28 14:43:49 +0800

    stash

--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import routes from './router'
 import store from './store'
-import {createRouter, createWebHistory} from 'vue-router'
-import STree from '@/components/s-tree/index'
- 
+import { createRouter, createWebHistory } from 'vue-router'
+import SUI from 's-ui'
+
 // createApp(App).use(store).use(router).mount('#app')
 
 
 let instance = null
 let router = null
 
-function render ({container} = {}) {
-    instance = createApp(App).use(STree)
+function render({ container } = {}) {
+    instance = createApp(App).use(SUI)
     router = createRouter({
         history: createWebHistory(window.__POWERED_BY_QIANKUN__ ? '/comp2' : ''),
         routes
@@ -27,15 +27,15 @@ if (!window.__POWERED_BY_QIANKUN__) {
     render()
 }
 
-export async function bootstrap () {
+export async function bootstrap() {
     console.info('vue3 bootstrap')
 }
 
-export async function mount (props) {
+export async function mount(props) {
     render(props)
 }
 
-export async function unmount () {
+export async function unmount() {
     instance.unmount()
     instance._container.innerHTML = ''
     instance = null
